1448f26edc0b98bfab504b5930761684d8ec2cc2
skhairlight <skhairlight@gmail.com>
2025-09-14 10:39:09 -0500

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,13 +1,30 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name: Salsabiel Khair Allah
+#Date: Sep.14
+#Assignment: Lab 3
 import random
 
 def main():
   wins = 0
   ties = 0
   losses = 0
+
+  choices = {"R": "Rock", "P": "Paper", "S": "Scissors"}
+
+  play_again = "Y"
+  while play_again.upper() == "Y": 
+    computer = random.choice(list(choices.key()))
+    player = input("Select Rock, Paper, or Scissors (R, P, S): ").upper()
+    if player not in choices:
+      print("Invalid choice. Try again.")
+      continue
+    print(f"Player chose: {choices[player]}")
+    print(f"Computer chose: {choices[computer]}")
+    if player == computer:
+      print("It's a tie!")
+      ties += 1
+    elif (player == "R" and computer == "S") or \
+    
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
 
