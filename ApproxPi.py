9bb6979d641892fc772158af8fa4342409751e89
skhairlight <skhairlight@gmail.com>
2025-09-14 10:31:09 -0500

    Update ApproxPi.py

--- ApproxPi.py
+++ ApproxPi.py
@@ -1,7 +1,7 @@
 #ApproxPi.py
-#Name:
-#Date:
-#Assignment:
+#Name: Salsabiel Khair Alla
+#Date: Sep.14
+#Assignment: Lab 3
 import math
 import time
 
@@ -12,14 +12,28 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
+  digits = int(input("How many decimal points to compute (0 - 10): "))
+  if digits < 0 or digits > 10:
+    print("Please enter a value between 0 and 10.")
+    return
 
   start = time.time()
   #calculate pi using the approximation technique
+  pi_approx = 0.0
+  sign = 1
+  denominator = 1
+  
   #Loop until the level of percision is reached
+  while round(pi_approx, digits) != round(realPi, digits):
+    pi_approx += sign * (4 / denominator)
+    sign *= 1
+    denominator += 2
 
   end = time.time()
 
   elapsedTime = end - start
+  print(f"Pi: {round(pi_approx, digits)}")
+  print(f"Calculated in {elapsedTime} seconds.")
   print(elapsedTime)
 
 if __name__ == '__main__':
